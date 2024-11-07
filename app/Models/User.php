ccac62425a448106c1ce4d6a82f92cff4d74b545
Matthew Speake <87565429+mspeake161@users.noreply.github.com>
2024-11-07 13:42:58 +0000

    Updated users table
    added necessary fields

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,14 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
         'email',
         'password',
+        'home_address',
+        'phone_number',
+        'postcode',
+        'role'
     ];
 
     /**
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,12 +13,17 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->text('home_address');
+            $table->string('phone_number');
+            $table->string('postcode');
             $table->rememberToken();
             $table->timestamps();
+            $table->string('role');
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
