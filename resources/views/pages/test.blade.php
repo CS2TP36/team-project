241a28984e25c986dbcb98075be7402f31f268d3
Matthew Speake <87565429+mspeake161@users.noreply.github.com>
2024-11-07 11:33:10 +0000

    Created test page to show routing

---
+++ resources/views/pages/test.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<head>
+    <title>Test page</title>
+</head>
+
+<body>
+    <h1>A Page to test routing</h1>
+    <p>If this is visible in the browser this has worked.</p>
+</body>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// page can be accessed at site/test
+Route::get('/test', function () {
+    return view('pages.test');
+});
