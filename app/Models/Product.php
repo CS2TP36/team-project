2786de7ed091adc2d21aa3c61936997293fd55f4
Matthew Speake <87565429+mspeake161@users.noreply.github.com>
2024-11-11 20:41:52 +0000

    Created Product Images table
    Migration and model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -25,5 +25,8 @@ class Product extends Model
     public function reviews() {
         return $this->hasMany(Review::class);
     }
+    public function images() {
+        return $this->hasMany(ProductImage::class);
+    }
 
 }
---
+++ app/Models/ProductImage.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductImage extends Model
+{
+    protected $table = 'product_images';
+    protected $primaryKey = 'id';
+    public $timestamps = true;
+    protected $fillable = [
+        'product_id',
+        // this will be the name of the image file located in the images directory
+        'image_name'
+    ];
+    public function product() {
+        return $this->belongsTo('App\Models\Product');
+    }
+}
