4be1de7e19d2338881f7a785cec6e1f25ee1a087
Kacper69pl <230146525@aston.ac.uk>
2024-11-13 22:34:56 +0000

    created a shipping table migration
    created a shipping table migration with all the relevant table fields with hopefully correct data types.

---
+++ database/migrations/2024_11_13_220855_create_shipping_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Order;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('shipping', function (Blueprint $table) {
+            $table->id();
+            $table->date('shipping_date');
+            $table->date('delivery_date');
+            $table->string('home_address');
+            $table->string('tracking_number');
+            $table->foreignIDFor(Order::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('shipping');
+    }
+};
