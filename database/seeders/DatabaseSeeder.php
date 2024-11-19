ba463c27e7c9a4188fec2ecd0c8e3219935179b4
Matthew Speake <matthew@speake.uk>
2024-11-19 17:06:41 +0000

    Created seeder for Categories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use App\Models\User;
-
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,11 +14,41 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // Adds required categories
+        $this->addCategories();
+    }
+    private function addCategories()
+    {
+        //shoes, trousers, hoodies, jackets, shirts
+        //creates a load of categories to add to the database.
+        $categories = [
+            new Category([
+                'name' => 'Shoes',
+                'description' => 'A selection of shoes'
+            ]),
+            new Category([
+                'name' => 'Trousers',
+                'description' => 'A selection of trousers'
+            ]),
+            new Category([
+                'name' => 'Hoodies',
+                'description' => 'A selection of hoodies'
+            ]),
+            new Category([
+                'name' => 'Jackets',
+                'description' => 'A selection of jackets'
+            ]),
+            new Category([
+                'name' => 'Shirts',
+                'description' => 'A selection of shirts'
+            ])
+        ];
+        // individually saves each category.
+        foreach ($categories as $category) {
+            $category->save();
+        }
+    }
+    private function addProducts() {
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
     }
 }
