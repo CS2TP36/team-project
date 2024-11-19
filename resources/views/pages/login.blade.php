b8294c214a2db5af76751f9d832914b2fa79090e
rikeshpatel19 <rkpatel1738@gmail.com>
2024-11-19 11:06:34 +0000

    Login Page CSS
    Trying out different styles to determine which one we want to keep moving forward.

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -3,16 +3,18 @@
     Login
 @endsection
 @section('content')
+    
     <div class="login">
+        <body>
         <h2>Login</h2>
         <!-- Need to work out how to use form method with Laravel-->
         <form method="post" action="">
             Username: <input type="text" name="username"/><br>
             Password: <input type="password" name="password"/><br><br>
             <input type="submit" value="Login"/>
-            <input type="reset" value="Clear"/>
-            <!--Need to link to registeration page-->
-            <a href="/register"><p>Not registered yet?</p></a>
+             <!--Links to registeration page-->
+            <a href="/register"><input type="register" value="Register"/></a>
         </form>
+        </body>
     </div>
 @endsection
