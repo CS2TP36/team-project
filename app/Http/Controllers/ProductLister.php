5313c4b6b71dffd5fcafb097b97bd4d56be0466a
Matthew Speake <matthew@speake.uk>
2024-12-01 18:26:50 +0000

    Update ProductLister.php
    Fixed category filter

--- app/Http/Controllers/ProductLister.php
+++ app/Http/Controllers/ProductLister.php
@@ -9,12 +9,12 @@ use Illuminate\Http\Request;
 class ProductLister extends Controller
 {
     // function which gets filtered items from the database, then sorts them and finally returns them in an array.
-    public static function get(bool $mens=true, string $sortBy="id", bool $ascending=true, string $catFilter="") {
+    public static function get(bool $mens=true, string $sortBy="id", bool $ascending=true, int $catFilter=0) {
             // get all the products from the db from selected gender
             $products = Product::all()->where('mens', $mens);
             // apply a category filter if there is one
-            if ($catFilter != "") {
-                $products = $products->where('category', $catFilter);
+            if ($catFilter != 0) {
+                $products = $products->where('category_id', $catFilter);
             }
             // sort by field and direction
             if ($ascending) {
@@ -26,7 +26,7 @@ class ProductLister extends Controller
             return $products;
     }
     // function for getting the args from the url and passing them back to the page
-    public function show(bool $mens=true, string $sortBy="id", bool $ascending=true, string $catFilter="") {
+    public function show(bool $mens=true, string $sortBy="id", bool $ascending=true, int $catFilter=0) {
         // returns the sanitised args to the page
         return view('pages.products', ['mens' => $mens, 'ascending' => $ascending, 'sortBy' => $sortBy, 'catFilter' => $catFilter]);
     }
