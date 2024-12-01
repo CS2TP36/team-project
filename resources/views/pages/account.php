132601e56bc2fc97c7f338c34a8f6f098eaa0ace
AshfaqC <ashfaqc@outlook.com>
2024-12-01 21:47:36 +0000

    Update account.php

--- resources/views/pages/account.php
+++ resources/views/pages/account.php
@@ -1,17 +1,22 @@
-@extends('layouts.page')
-@section('title','Account')
-@section('content')
-
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My account</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+   
     <div id="account-page">
     <h1>My Account</h1>
     <hr>
-    <p><strong>Name:</strong>John Smith</p>
+    <p><strong>Name:</strong> John Smith</p>
     <hr>
-    <p><strong>Email:</strong>j.smith@example.com</p>
+    <p><strong>Email:</strong> j.smith@example.com</p>
     <hr>
-    <button type="button"> Change password </button>
-</div>
-@endsection
-
+    <button type="button">Change password</button>
     
+</body>
+</html>
+
