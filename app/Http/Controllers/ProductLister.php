2bc5835d17737807fedfe2985bd11e206db3cea4
Matthew Speake <matthew@speake.uk>
2024-12-01 18:39:52 +0000

    Update ProductLister.php
    allow for mixed genders to be shown

--- app/Http/Controllers/ProductLister.php
+++ app/Http/Controllers/ProductLister.php
@@ -9,9 +9,13 @@ use Illuminate\Http\Request;
 class ProductLister extends Controller
 {
     // function which gets filtered items from the database, then sorts them and finally returns them in an array.
-    public static function get(bool $mens=true, string $sortBy="id", bool $ascending=true, int $catFilter=0) {
+    public static function get(int $mens=2, string $sortBy="id", bool $ascending=true, int $catFilter=0) {
             // get all the products from the db from selected gender
-            $products = Product::all()->where('mens', $mens);
+            if ($mens!=2) {
+                $products = Product::all()->where('mens', $mens);
+            } else {
+                $products = Product::all();
+            }
             // apply a category filter if there is one
             if ($catFilter != 0) {
                 $products = $products->where('category_id', $catFilter);
@@ -26,7 +30,7 @@ class ProductLister extends Controller
             return $products;
     }
     // function for getting the args from the url and passing them back to the page
-    public function show(bool $mens=true, string $sortBy="id", bool $ascending=true, int $catFilter=0) {
+    public function show(int $mens=2, string $sortBy="id", bool $ascending=true, int $catFilter=0) {
         // returns the sanitised args to the page
         return view('pages.products', ['mens' => $mens, 'ascending' => $ascending, 'sortBy' => $sortBy, 'catFilter' => $catFilter]);
     }
