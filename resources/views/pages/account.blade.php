63710527168095300ac961cfc6253432e7259710
Matthew Speake <matthew@speake.uk>
2024-12-02 14:38:23 +0000

    Update account.blade.php
    added some php to get fields

--- resources/views/pages/account.blade.php
+++ resources/views/pages/account.blade.php
@@ -2,19 +2,19 @@
 @section('title','Account')
 @section('content')
 
-   
+
     <div id="account-page">
-    
-    
+
+
     <h1>My Account</h1>
     <hr>
-    <p><strong>Name:</strong> John Smith</p>
+    <p><strong>Name:</strong> {{$user['first_name']}} {{$user['last_name']}}</p>
     <hr>
-    <p><strong>Email:</strong> j.smith@example.com</p>
+    <p><strong>Email:</strong> {{$user['email']}}</p>
     <hr>
     <button type="button">Change password</button>
 
     </div>
-    
+
 @endsection
 
