c2b32656981d8cfc265a38d318164cef97c0461b
Matthew Speake <matthew@speake.uk>
2024-12-06 17:30:39 +0000

    Change default theme to light
    because this dark mode is horrible

--- public/js/theme.js
+++ public/js/theme.js
@@ -1,7 +1,9 @@
 const themeToggle = document.getElementById("theme-toggle");
 let dark = true;
+// switches between the 2 themes in css
 function toggleTheme() {
     dark = !dark;
+    // ensures the cookie is updated
     if (dark) {
         setTheme("dark");
     } else {
@@ -12,12 +14,13 @@ function toggleTheme() {
     body.classList.toggle("dark-theme");
 }
 
+// runs when any page loaded
 document.addEventListener('DOMContentLoaded', () => {
     dark = true;
     if (getTheme() === 'light'){
         toggleTheme();
     }
-    // console.log(dark);
+    // when the button is pressed needs to swap themes
     const themeToggle = document.getElementById('theme-toggle');
     themeToggle.addEventListener('click', toggleTheme);
 });
@@ -25,17 +28,18 @@ document.addEventListener('DOMContentLoaded', () => {
 function setTheme(value){
     document.cookie = `theme=${value}; path=/`;
 }
-
+// gets pre-set theme from the cookie, if none exists set to light (looks a lot better)
 function getTheme() {
+    // gets the value from the cookie
     let cookie = document.cookie.split(';')[0];
     let theme = cookie.split('=')[1];
-    // console.log(theme);
     if (theme === 'dark') {
         return 'dark';
     } else if (theme === 'light') {
         return 'light';
     } else {
-        setTheme("dark");
-        return 'dark';
+        // sets light by default if none selected
+        setTheme("light");
+        return 'light';
     }
 }
