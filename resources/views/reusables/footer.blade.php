415029a344ee578a46624ea2593310246bff29a9
Matthew Speake <matthew@speake.uk>
2024-12-06 21:39:23 +0000

    Update footer.blade.php

--- resources/views/reusables/footer.blade.php
+++ resources/views/reusables/footer.blade.php
@@ -2,28 +2,28 @@
     <div class="footer1">
         <ul>
             <li class="footer-list"><a href="/contact">Contact: 0121 010 0111</a></li>
-            <li class="footer-list">Address: Aston University</a></li>
-            <li class="footer-list">Open Hours: 6:00 - 23:00</a></li>
-            <li class="footer-list">Follow Our Socials</a></li>  
+            <li class="footer-list">Address: Aston University</li>
+            <li class="footer-list">Open Hours: 6:00 - 23:00</li>
+            <li class="footer-list">Follow Our Socials</li>
         </ul>
         <ul class="footer-media-container">
             <li class="footerprime-list"><a href="https://x.com/" target= "_blank"><img src="{{asset('images/twitter-removebg-preview.png')}}" alt="Twitter"></a></li>
             <li class="footerprime-list"><a href="https://facebook.com" target="_blank"><img src="{{asset('images/111.png')}}" alt="Facebook"></a></li>
             <li class="footerprime-list"><a href="https://instagram.com" target="_blank"><img src="{{asset('images/instagram-logo-transparent-background-2.png')}}" alt="Instagram"></a></li>
-        </ul>                                      
-    </div>
-    <div>
-        <ul>
-            <li class="footer-list"><a href="/aboutus">About Us</a></li> 
-            <li class="footer-list">Privacy</a></li>
-            <li class="footer-list">Terms and condition</a></li>
         </ul>
     </div>
     <div>
         <ul>
-            <li class="footer-list">Page Directs</a></li>
+            <li class="footer-list"><a href="/aboutus">About Us</a></li>
+            <li class="footer-list">Privacy</li>
+            <li class="footer-list">Terms and condition</li>
+        </ul>
+    </div>
+    <div>
+        <ul>
+            <li class="footer-list">Page Directs</li>
             <li class="footer-list"><a href="/login">Sign in </a></li>
-            <li class="footer-list">Orders and Payments </a></li>
+            <li class="footer-list">Orders and Payments</li>
             <li class="footer-list"><button id="theme-toggle">Toggle Theme</button></li>
         </ul>
     </div>
