c4d0d5b7e49e14b42c582db957f85c4219e95b74
K3YC0D3 <firstnamesurename8@gmail.com>
2024-12-08 21:27:15 +0000

    Create terms&conditions.blade.php
    terms and conditions

---
+++ resources/views/pages/terms&conditions.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.page')
+@section ('title' ,'Terms and Conditions' )
+@section ('content')
+<div class = "Terms and Conditions">
+    <section>
+        <div id ="text-container"> 
+            <p> TERMS AND CONDITIONS<P>
+        
+        <div>
+            <section>
+
+
+
+
+
+
+
+
+<div>
+            
