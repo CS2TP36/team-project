2e934caf2e4d8f8fec5796d31a567acf36c61316
K3YC0D3 <firstnamesurename8@gmail.com>
2024-12-08 22:49:58 +0000

    terms and conditions

--- resources/views/pages/terms-conditions.blade.php
+++ resources/views/pages/terms-conditions.blade.php
@@ -4,7 +4,28 @@
 <div class = "Terms and Conditions">
     <section>
         <div id ="text-container">
-            <p> TERMS AND CONDITIONS<P>
+            <h1> TERMS AND CONDITIONS</h1>
+            
+            <p>These Terms and Conditions outline the rules and regulations for the use of our e-commerce platform. By accessing or using our website, you agree to comply with and be bound by these terms.</p>
+            <h2>Genral Terms</h2>
+            <P>By using this site, you confirm that you are at least 18 years old or accessing with the consent of a legal guardian.
+            We reserve the right to modify these terms at any time. Updates will be posted on this page, and continued use of the website constitutes acceptance of the revised terms.
+            </P>
+            <h2>Account Registration</h2>
+            <p>To access certain features, you must create an account. You are responsible for maintaining the confidentiality of your account credentials.
+            You agree to provide accurate and complete information during registration and to update it as necessary.
+            We reserve the right to suspend or terminate accounts that violate these terms.</p>
+            <h2> Product info and Availabilty </h2>
+            <p> All products are described as accurately as possible. However, we do not guarantee that descriptions, images, or other content are error-free or complete.
+            Product availability is subject to change without notice. We reserve the right to limit quantities or discontinue products.
+            Prices are subject to change at any time but will be honored at the rate displayed during checkout.</P>
+            <h2> Orders and payment</h2>
+            <p> By placing an order, you agree to provide current, complete, and accurate payment information.
+            We reserve the right to cancel or refuse orders for any reason, including suspected fraud or errors in pricing or availability.
+            Payment methods accepted include [e.g., credit cards, PayPal, etc.].
+
+
+
 
         <div>
             <section>
