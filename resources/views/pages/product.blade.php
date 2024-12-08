fe8d28c816bfe9c32e282a4bae2e1b565b2ef57c
rikeshpatel19 <rkpatel1738@gmail.com>
2024-12-08 16:07:07 +0000

    CSS Changes
    Attempting to make Dark mode better

--- resources/views/pages/product.blade.php
+++ resources/views/pages/product.blade.php
@@ -7,7 +7,6 @@
 @endphp
 @section('content')
     <div class="product">
-
         <div class = "left-container">
             <!-- Image of the product -->
             <img src="{{ asset($product->getMainImage()) }}"></img>
@@ -20,7 +19,6 @@
                 <!-- Price of the product -->
                 <h2>£{{$product['price']}}</h2>
             </div>
-
             <!--<div class = "size-selection">
                 <p>Choose size</p>
                 ( Three buttons, S, M, L )
@@ -29,35 +27,33 @@
                 <button type="button">L</button>
             </div> -->
             <div class="purchase-options">
-    <!-- Add to Basket -->
-    <form action="{{ route('basket.add') }}" method="POST">
-        @csrf
-        <input type="hidden" name="product_id" value="{{ $product->id }}">
-        <input type="hidden" name="size" id="size" value="">
-        <label for="quantity">Quantity:</label>
-        <input type="number" id="quantity" name="quantity" value="1" min="1"> ({{$product['stock']}} in stock)
-        <p>Choose size:</p>
-        <button class = "size-selection" type="button" onclick="selectSize('S')">S</button>
-        <button class = "size-selection" type="button" onclick="selectSize('M')">M</button>
-        <button class = "size-selection" type="button" onclick="selectSize('L')">L</button>
-        <button type="submit">Add to Basket</button>
-    </form>
+                <!-- Add to Basket -->
+                <form action="{{ route('basket.add') }}" method="POST">
+                    @csrf
+                    <input type="hidden" name="product_id" value="{{ $product->id }}">
+                    <input type="hidden" name="size" id="size" value="">
+                    <label for="quantity">Quantity:</label>
+                    <input type="number" id="quantity" name="quantity" value="1" min="1"> ({{$product['stock']}} in stock)
+                    <p>Choose size:</p>
+                    <button class = "size-selection" type="button" onclick="selectSize('S')">S</button>
+                    <button class = "size-selection" type="button" onclick="selectSize('M')">M</button>
+                    <button class = "size-selection" type="button" onclick="selectSize('L')">L</button>
+                    <button type="submit">Add to Basket</button>
+                </form>
+                <!-- Add to Wishlist
+                <button type="button">Add to Wishlist</button>
+                -->
+            </div>
 
-    <!-- Add to Wishlist
-    <button type="button">Add to Wishlist</button>
-    -->
-</div>
-
-<script>
-    function selectSize(size) {
-        document.getElementById('size').value = size;
-    }
-</script>
+            <script>
+                function selectSize(size) {
+                    document.getElementById('size').value = size;
+                }
+            </script>
 
             <div class = "detailed-info">
                 <!-- Nav bar containing Product Info and Review-->
                 <nav></nav>
-
                 <!-- Description of the product -->
                 <p><strong>Description:</strong></p>
                 <p id="show_description">{{$product['description']}}</p>
