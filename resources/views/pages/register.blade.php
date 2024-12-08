d25108238e61da11f5a453ce01642a38487479b2
LOKI-HGR <h.afzal.786@outlook.com>
2024-12-08 20:23:36 +0000

    Phone Number Change
    sds

--- resources/views/pages/register.blade.php
+++ resources/views/pages/register.blade.php
@@ -45,8 +45,8 @@
             
             
             <label for="phone">Phone Number:</label>
-            <input type="tel" id="phone" name="phone" value="{{ old('phone') }}" required pattern="^\d{3,15}$" placeholder="Enter your 3-15 Digit Phone Number">
-            <span id="phoneError" class="error"></span>
+            <input type="tel" id="phone" name="phone" required pattern="^\+44\d{10,13}$" placeholder="e.g., +44 1234 567890 (Needs to be UK)">  
+            <span id="phoneError" class="error"></span>  
 
 
             <label for="address">Address:</label>
