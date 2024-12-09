d58215e9cd6ec6b2152435027f683a781893270b
ShasankThapa <230113480@aston.ac.uk>
2024-12-09 13:07:48 +0000

    Changed the about us Page
    Changed the about us Page writing

--- resources/views/pages/aboutus.blade.php
+++ resources/views/pages/aboutus.blade.php
@@ -11,17 +11,17 @@
     Our mission is to support athletes and fitness enthusiasts by offering products that
     combine style, performance and comfort. With a commitment to excellence, we strive
     to meet the needs of our customers, helping them reach their goals while looking
-    and feeling their best.
+    and feeling their best. 
 </p>
 
 <p> Our collection is crafted with attention to detail, ensuring every item delivers both
     durability and innovation. Whether you're going to the gym, outdoors for a run, or
-    playing your favourite sport, we have you covered with the best sportswear.
+    playing your favourite sport, we have you covered with the best sportswear. 
 </p>
 
 <p>At the core of the business is a dedication to customer satisfaction. We believe in
     continuous improvement and innovation, aiming to provide exceptional value in every
-    product we offer.
+    product we offer. 
 </p>
 
 <h2>Our History?</h2>
@@ -29,7 +29,7 @@
 <p>Our journey began in the bustling halls of university, where nine of us first crossed
     paths as classmates, teammates, and eventually close friends. We worked together on
     countless projects and through brainstorming, we learnt that our combined talents
-    could create something bigger than ourselves.
+    could create something bigger than ourselves. 
 </p>
 
 
