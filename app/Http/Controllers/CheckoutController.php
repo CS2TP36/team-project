52f613c4095fc0b1b206025b081c93ec15737957
Matthew Speake <matthew@speake.uk>
2024-12-09 00:42:44 +0000

    Remove basket items after checkout

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\IndividualOrder;
 use Illuminate\Http\Request;
 use App\Models\Order;
 use App\Models\Transaction;
@@ -31,15 +32,16 @@ class CheckoutController extends Controller
         DB::beginTransaction();
 
         try {
+            // get values from the request
             $values = $request->only([
                 'region', 'full_name', 'address', 'postcode', 'phone',
                 'card_name', 'card_number', 'expiry_date', 'cvv'
             ]);
-
+            // get relevent basket items
             $basket = Basket::where('user_id', Auth::id())->get();
-
+            // get the total price
             $total = $basket->sum(fn($item) => $item->getTotalPrice());
-
+            // create a new order
             $order = Order::create([
                 'user_id' => Auth::id(),
                 'order_date' => now(),
@@ -48,16 +50,18 @@ class CheckoutController extends Controller
                 'shipping_id' => null,
             ]);
 
-
+            // create a transaction for the order
             $transaction = Transaction::create([
                 'transaction_amount' => $total,
                 'transaction_info' => 'purchase',
                 'transaction_status' => 'completed',
             ]);
 
+            // link the transaction to the order
             $order->transaction_id = $transaction->id;
             $order->save();
 
+            // create the shipping item
             $shipping = Shipping::create([
                 'shipping_date' => now(),
                 'delivery_date' => null,
@@ -65,9 +69,29 @@ class CheckoutController extends Controller
                 'tracking_number' => rand(100000, 999999),
             ]);
 
+            // link shipping to order
             $order->shipping_id = $shipping->id;
             $order->save();
 
+            // convert every basket item into and individualOrder item
+            /*
+            foreach ($basket as $basketItem) {
+                $individualOrder = new IndividualOrder([
+                    'order_id' => $order->id,
+                    'product_id' => $basketItem->product->id,
+                    'quantity' => $basketItem->quantity,
+                    'price' => $basketItem->product->price,
+                    'size' => $basketItem->size
+                ]);
+                $individualOrder->save();
+            }
+               */
+            // delete all the basket items for that user
+            foreach ($basket as $item) {
+                $item->delete();
+            }
+
+            // commit to db
             DB::commit();
 
             return view('pages.success')->with(['orderNumber' => $order['id'], 'trackingNumber' => $shipping['tracking_number']]);
@@ -75,9 +99,7 @@ class CheckoutController extends Controller
         } catch (\Exception $e) {
             DB::rollBack();
 
-
-
-            return redirect()->route('checkout')->withErrors('An error occurred. Please try again later.');
+            return redirect()->route('checkout.checkout')->withErrors('An error occurred. Please try again later.');
         }
     }
 }
