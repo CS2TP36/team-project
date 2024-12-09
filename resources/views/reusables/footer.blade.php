4b64235977884619affef103ef12ef86dbc77f10
Matthew Speake <matthew@speake.uk>
2024-12-09 10:20:32 +0000

    Remove more footer

--- resources/views/reusables/footer.blade.php
+++ resources/views/reusables/footer.blade.php
@@ -2,21 +2,18 @@
 <footer class="footer-check">
         <ul>
             <li class="footer-list"><a href="/contact">Contact Page</a></li>
-            <li class="footer-list">Address: Aston University</li>
-            <li class="footer-list">Open Hours: 6:00 - 23:00</li>
+            <li class="footer-list"><a href="https://maps.app.goo.gl/vJZFAyvd6BEAWSWdA">Address: Aston University</a></li>
         </ul>
 
     <div>
         <ul>
             <li class="footer-list"><a href="/aboutus">About Us</a></li>
             <li class="footer-list"><a href="/terms-conditions">Terms and condition</a></li>
-            <li class="footer-list">Privacy</li>
         </ul>
     </div>
     <div>
         <ul>
             <li class="footer-list"> @if(!Auth::check()) <a href="/login">Sign in @else <a href="/logout"> Sign out @endif</a></li>
-            <li class="footer-list">Orders and Payments</li>
             <li class="footer-list"><button id="theme-toggle">Toggle Theme</button></li>
         </ul>
     </div>
