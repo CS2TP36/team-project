b932c70fd3c1eae6a4f157953a622ba23e126ce7
Matthew Speake <matthew@speake.uk>
2025-01-31 13:33:52 +0000

    fixing tailwind

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,6 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss(),
+        tailwindcss,
     ],
 });
