5945f2437f19c0d543efcbe63ed33f64af17aeb5
ShasankThapa <230113480@aston.ac.uk>
2025-02-04 21:51:44 +0000

    Header and Search Bar Complete Change
    I have completely Changed the layout of the header, this makes it look very clean, with most of the buttons for account and basket being changed to icons now. The search bar is improved, moved to the right made nicer. The logo has been cleaned up with an additional writing added on it

--- resources/views/reusables/header.blade.php
+++ resources/views/reusables/header.blade.php
@@ -1,12 +1,16 @@
 <header> 
     <nav class="navbar"> <!--Creates the navbar class creating a container-->
         <div class="navbardiv"><!--Is the div which will contain all the items that are present on the logo-->
-            <a href="/home"><img src="{{ asset('images/Logo.JPG') }}" class="logo" alt="logo"></a>
-            <input id="search-bar" type="search" placeholder="Search Here" onsearch="search()"> </input><!--search bar gets created-->
+            <a href="/home"><img src="{{ asset('images/Brand.png') }}" class="logo" alt="logo"></a>
             <ul>
                 <li class=prime-list><!--class holds the account and basket links-->
-                    <a href="/account">Account</a><!--links to account page-->
-                    <a href="/basket">Basket</a><!--links to basket page-->
+                    <div class="searchbar-container">
+                        <input id="search-bar" type="search" placeholder="Search SportsWear" onsearch="search()">
+                        <img src="{{ asset('images/search.png') }}" class="search-logo" alt="search-logo">
+                    </div>
+                    <a href="/account"><img src="{{ asset('images/Account.png') }}" class="user-logo" ></a> <!--links to account page-->
+                    <a href="/home"><img src="{{ asset('images/Hearts.png') }}" class="user-logo" > </a>
+                    <a href="/basket"><img src="{{ asset('images/Shop.png') }}" class="user-logo" > </a> <!--links to basket page-->
                 </li>
             </ul>
         </div>
