d0c3fff990d92ca4ee783f48e62f1426aab2be2a
ShasankThapa <230113480@aston.ac.uk>
2025-02-05 15:40:36 +0000

    Catogery divider changes
    Made some stylistic changes in consideration to the catogery divider. I have add a lightish blue background to it  and I have also changed the border radius and thickness of it.

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -6,9 +6,9 @@
         <div id="banner-container"><!--creates the banner container-->
             <img src="{{asset('images/Banner.png')}}" id="banner">
             <div id="text-banner">
-            <p>Dream</p>
-            <p>Aspire Higher</p>
-            <p>Achieve Your Destiny</p>
+            <p>Dream.</p>
+            <p>Aspire Higher.</p>
+            <p>Achieve Your Destiny.</p>
                 <!--<p>The Best Quality For The Best Price</p> -->
                 <!--<a href="/products">Shop Now</a>-->
             </div>
@@ -21,23 +21,23 @@
 
     <div id="category-section"> <!--shows the sections for each catogery-->
         <ul>
-            <li id="select-catogeries"><img src="{{asset('images/coat.jpg')}}"> <a href="/products/2/name/1/4">Jackets</a></li><!--links to coats-->
-            <li id="select-catogeries"><img src="{{asset('images/hoodies.jpg')}}"> <a href="/products/2/name/1/3">Hoodies</a></li><!--links to hoodies-->
-            <li id="select-catogeries"><img src="{{asset('images/Joggers.jpg')}}"> <a href="/products/2/name/1/2">Joggers</a></li><!--links joggers -->
-            <li id="select-catogeries"><img src="{{asset('images/shoes.jpg')}}"> <a href="/products/2/name/1/1">Trainers</a></li><!--links to shoes -->
-            <li id="select-catogeries"><img src="{{asset('images/shirt.jpg')}}"> <a href="/products/2/name/1/5">Shirts</a></li><!--links to shirts-->
+            <li id="select-catogeries"><img src="{{asset('images/coat-cat.png')}}"> <a href="/products/2/name/1/4">Jackets</a></li><!--links to coats-->
+            <li id="select-catogeries"><img src="{{asset('images/hoodie-cat.png')}}"> <a href="/products/2/name/1/3">Hoodies</a></li><!--links to hoodies-->
+            <li id="select-catogeries"><img src="{{asset('images/Joggers-cat.png')}}"> <a href="/products/2/name/1/2">Joggers</a></li><!--links joggers -->
+            <li id="select-catogeries"><img src="{{asset('images/shoe-cat.png')}}"> <a href="/products/2/name/1/1">Trainers</a></li><!--links to shoes -->
+            <li id="select-catogeries"><img src="{{asset('images/shirt-cat.png')}}"> <a href="/products/2/name/1/5">Shirts</a></li><!--links to shirts-->
         </ul>
     </div>
 
-    <div id=catogery-divider>
+    <!--<div id=catogery-divider>
         <p>Gender</p>
     </div>
 
-    <div id="gender-section"><!--shows the section for genders-->
+    <div id="gender-section"><!--shows the section for genders
         <ul>
-            <li id="select-catogeries"><img src="{{asset('images/Man.jpg')}}"> <a href="/products/1">Men</a></li><!--links for men-->
-            <li id="select-catogeries"><img src="{{asset('images/Woman.jpg')}}"> <a href="/products/0">Women</a></li><!--links for women-->
+            <li id="select-catogeries"><img src="{{asset('images/Man.jpg')}}"> <a href="/products/1">Men</a></li>
+            <li id="select-catogeries"><img src="{{asset('images/Woman.jpg')}}"> <a href="/products/0">Women</a></li>
         </ul>
     </div>
-</div>
-@endsection<!--ends section layout-->
+</div> -->
+@endsection
