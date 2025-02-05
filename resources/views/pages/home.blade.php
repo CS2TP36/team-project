114bbd804f18f2f4feb435f19ac5127c663bf19c
ShasankThapa <230113480@aston.ac.uk>
2025-02-05 00:27:56 +0000

    Header Animation and fixes, home page fixes
    I made the header animation and made some major home page banner and colour changes

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,10 +4,13 @@
 <div class= "home"><!--creates the home class-->
     <section>
         <div id="banner-container"><!--creates the banner container-->
-            <img src="{{asset('images/Hero-banner.jpeg')}}" id="banner">
+            <img src="{{asset('images/Banner.png')}}" id="banner">
             <div id="text-banner">
-                <p>The Best Quality For The Best Price</p>
-                <a href="/products">Shop Now</a>
+            <p>Dream</p>
+            <p>Aspire Higher</p>
+            <p>Achieve your Destiny</p>
+                <!--<p>The Best Quality For The Best Price</p> -->
+                <!--<a href="/products">Shop Now</a>-->
             </div>
         </div>
     </section>
