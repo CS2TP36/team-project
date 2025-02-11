e9e80935b0c94123ad4725a35a99a64655f5d17e
Rikesh Patel <rkpatel1738@gmail.com>
2025-02-11 19:43:52 +0000

    Stars on Review Page
    Started on star rating.

--- resources/views/pages/review.blade.php
+++ resources/views/pages/review.blade.php
@@ -10,6 +10,14 @@
     <br>
     <h2>Overall rating</h2>
     <!-- Stars -->
+    <div class="rating" data-rating="0">
+        <span class="star" data-value="1">&#9734;</span> 
+        <span class="star" data-value="2">&#9734;</span>
+        <span class="star" data-value="3">&#9734;</span>
+        <span class="star" data-value="4">&#9734;</span>
+        <span class="star" data-value="5">&#9734;</span>    
+    </div>
+    <script src="{{ asset('js/rating.js') }}"></script>
     <br>
     <h2>Add a headline</h2>
     <!-- Input for headline -->
