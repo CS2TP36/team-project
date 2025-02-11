8487a0d371cdb0bdb36068dad9ec7dae07f20ad1
Matthew Speake <matthew@speake.uk>
2025-02-11 17:32:14 +0000

    repeatedly flipping arrays to try and achieve something

--- app/Charts/StockSalesChart.php
+++ app/Charts/StockSalesChart.php
@@ -2,6 +2,7 @@
 
 namespace App\Charts;
 
+use App\Models\IndividualOrder;
 use App\Models\Product;
 use marineusde\LarapexCharts\Charts\LineChart AS OriginalLineChart;
 use marineusde\LarapexCharts\Options\XAxisOption;
@@ -15,12 +16,28 @@ class StockSalesChart
         $last14days = [now()->day];
         for ($i = 1; $i < 14; $i++) {
             $last14days[] = now()->subDays($i)->day;
-        }
+        } // [14,13,12,11,10,9,8,7,6,5,4,3,2,1]
+        $last14days = array_reverse($last14days); // [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+        // get number of sales from each of the last 14 days
+        $salesPerDay = [];
+        foreach ($last14days as $day) {
+            $salesPerDay[] = IndividualOrder::all()->where('product_id', $product->id)->where('created_at', $day)->count();
+        } // [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+        // now roughly calculate the stock level for each of the last 14 days
+        $salesPerDay = array_reverse($salesPerDay); // [14,13,12,11,10,9,8,7,6,5,4,3,2,1]
+        $stockCount = $product->stock;
+        $stockLevel = [$stockCount];
+        foreach ($salesPerDay as $sales) {
+            $stockCount += $sales;
+            $stockLevel[] = $stockCount;
+        } // [14,13,12,11,10,9,8,7,6,5,4,3,2,1]
+        $stockLevel = array_reverse($stockLevel); // [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
 
+        // returns the built chart with the required data
         return (new OriginalLineChart)
             ->setTitle('Recent Sales for ' . $product->name)
             ->setSubtitle('Last 14 days')
-            ->addData('Stock level', [/*TODO: add stock level data*/])
+            ->addData('Stock level', $stockLevel)
             ->setXAxisOption(new XAxisOption($last14days));
     }
 }
