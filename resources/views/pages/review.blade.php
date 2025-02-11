c0655c84153e60070d72909da86795df32e34fb6
Rikesh Patel <rkpatel1738@gmail.com>
2025-02-11 14:45:46 +0000

    Review Page
    Started on the Review Page

---
+++ resources/views/pages/review.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.page')
+@section('title','Review')
+@section('content')
+<div class="review">
+    <!-- Need a form submission --> 
+    <h1>Create Review</h1>
+    <!-- Image + Name of Product -->
+    <img></img>
+    <p></p>
+    <br>
+    <h2>Overall rating</h2>
+    <!-- Stars -->
+    <br>
+    <h2>Add a headline</h2>
+    <!-- Input for headline -->
+    <input></input>
+    <br>
+    <h2>Add a written review</h2>
+    <!-- Input for review -->
+    <input></input>
+    <br>
+    <p>We will notify you via email as soon as your review is processed.<p>
+    <!-- Submit button -->
+    <button>Submit</button> 
+</div>
+@endsection
