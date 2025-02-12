71c71f2b20a6c5abdedaea22ea30d09fa7ba773c
Matthew Speake <matthew@speake.uk>
2025-02-12 16:33:46 +0000

    Fix search spaces

--- public/js/searchbar.js
+++ public/js/searchbar.js
@@ -3,7 +3,7 @@ function search() {
     console.log(document.getElementById('search-bar').value);
     let searchTerm = document.getElementById('search-bar').value;
     // convert space to #
-    searchTerm = searchTerm.replace(' ', '#');
+    searchTerm = searchTerm.replace(' ', '%20');
     searchTerm = searchTerm.replace('\\', '');
     // runs the search in a new window
     const url = `/search/${searchTerm}`;
