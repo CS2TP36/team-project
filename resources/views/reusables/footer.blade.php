186afdf38c16f72534e432af70679d3483b627d9
ShasankThapa <230113480@aston.ac.uk>
2025-02-12 19:47:54 +0000

    Footer HTML Change
    Change in Footer Html

--- resources/views/reusables/footer.blade.php
+++ resources/views/reusables/footer.blade.php
@@ -1,27 +1,38 @@
 @use(Illuminate\Support\Facades\Auth)
 <footer class="footer-container"><!--creates the footer class container that holds all the items inside as well acts as the container-->
         <ul><!--list for contacting us-->
-            <li class="footer-list"><a href="/contact">Customer Service</a></li><!--links to the contact page-->
-            <li class="footer-list"><a href="/contact">Contact Page</a></li><!--links to the contact page-->
-            <li class="footer-list"><a href="/aboutus">About Us</a></li><!--links to about us-->
-            <li class="footer-list"><a href="https://maps.app.goo.gl/vJZFAyvd6BEAWSWdA">Address: Aston University</a></li><!--links to google maps-->
+            <li class="footer-title">Customer Service</li><!--links to the contact page-->
+            <li class="footer-list"><a href="/contact">Contact Us</a></li><!--links to the contact page-->
+            <li class="footer-list"><a href="/aboutus">Return Policy</a></li><!--links to about us-->
+            <li class="footer-list"><a href="/aboutus">Shipping Information</a></li>
+            <li class="footer-list"><a href="/aboutus">FAQ</a></li>
         </ul>
 
     <div>
         <ul><!--list for about us/terms-->
+            <li class="footer-title">Company Info</li>
+            <li class="footer-list"><a href="/aboutus">About Us</a></li>
             <li class="footer-list"><a href="/terms-conditions">Terms and Conditions</a></li><!--links to the terms and conditions-->
-            <li class="footer-list"><a>Wishlist</a></li><!--links to about us-->
-            <li class="footer-list"><a>Track My Order</a></li><!--links to about us-->
-            <li class="footer-list"><a href="{{ route("delivery.returns") }}">Delivery and Returns</a></li><!--links to about us-->
+            <li class="footer-list"><a href = "/student">Student Discount</a></li><!--links to about us-->
+            <li class="footer-list"><a>Privacy Policy</a></li><!--links to about us-->
         </ul>
     </div>
 
     <div>
         <ul><!--list for signing in and toggle theme-->
+            <li class="footer-title">Account Info</li>
             <li class="footer-list"> @if(!Auth::check()) <a href="/login">Sign In @else <a href="/logout"> Sign Out @endif</a></li><!--sign in -->
-            <li class="footer-list"><a href="https://www.instagram.com/">Follow Us</a></li><!--links to the terms and conditions-->
-            <li class="footer-list"><a>Student Discount</a></li><!--links to the terms and conditions-->
+            <li class="footer-list"><a href="/wishlist">Wishlist</a></li>
             <li class="footer-list"><button id="theme-toggle">Toggle Theme</button></li><!--dark mode/light mode toggle theme-->
         </ul>
     </div>
+    <div>
+        <ul><!--list for signing in and toggle theme-->
+            <li class="footer-title">Follow Us</li>
+            <li class="footer-list"></li>
+            <li class="footer-list"></li>
+            <li class="footer-list"></li>
+        </ul>
+    </div>
+    
 </footer>
