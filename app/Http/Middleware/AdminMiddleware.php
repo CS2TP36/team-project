f016df461d8cc0bf212ded65beae0f96f9be07e6
Matthew Speake <matthew@speake.uk>
2025-02-16 22:09:56 +0000

    added product management abilities
    Added pages and backend for adding/deleting/modifying the products

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AdminMiddleware
+{
+    public function handle(Request $request, Closure $next)
+    {
+        return $next($request); // ✅ Allow anyone to access without checking admin status
+    }
+}
