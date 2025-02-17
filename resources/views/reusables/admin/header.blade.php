82190e72e587f45b7b2126b696cf0d7b5c8a6e22
Matthew Speake <matthew@speake.uk>
2025-02-17 12:10:30 +0000

    Add back manage users placeholder

--- resources/views/reusables/admin/header.blade.php
+++ resources/views/reusables/admin/header.blade.php
@@ -6,10 +6,10 @@
     <nav>
         <ul>
             <li><a href="{{ route('admin.account') }}">Account</a></li>
-            <li><a href="#">Manage Products</a></li>
             <li><a href="{{ route('admin.reports') }}">Stock Reports</a></li>
             @if(Auth::check())
                 <li><a href="{{ route('admin.products.index') }}" class="btn btn-primary">Manage Products</a></li>
+                <li><a href="#">Manage Users</a></li>
             @endif
             <li><a href="#">User messages</a></li>
         </ul>
