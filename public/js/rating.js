6e50d7f7641e8ab8919f7382960ab8c2d84c6b47
Matthew Speake <matthew@speake.uk>
2025-02-18 17:31:22 +0000

    reformat and add some php to reviews

--- public/js/rating.js
+++ public/js/rating.js
@@ -2,20 +2,21 @@ const ratingContainer = document.querySelector('.rating');
 const stars = ratingContainer.querySelectorAll('.star');
 
 stars.forEach(star => {
-  star.addEventListener('click', () => {
-    const rating = parseInt(star.dataset.value, 10);
-    ratingContainer.dataset.rating = rating;
+    star.addEventListener('click', () => {
+        const rating = parseInt(star.dataset.value, 10);
+        ratingContainer.dataset.rating = rating;
 
-    // Update star appearance
-    stars.forEach(s => {
-      if (s.dataset.value <= rating) {
-        s.classList.add('active');
-      } else {
-        s.classList.remove('active');
-      }
+        // Update star appearance
+        stars.forEach(s => {
+            if (s.dataset.value <= rating) {
+                s.classList.add('active');
+            } else {
+                s.classList.remove('active');
+            }
+        });
+
+        // You can now submit the rating to your server or perform other actions
+        console.log("Rating submitted:", rating);
     });
-
-    // You can now submit the rating to your server or perform other actions
-    console.log("Rating submitted:", rating);
-  });
 });
+
