0bb8772ab7d6df5e6d0fa69a7df1944a8b5031db
Matthew Speake <matthew@speake.uk>
2025-02-18 23:06:28 +0000

    Create route for privacy policy

--- resources/views/reusables/footer.blade.php
+++ resources/views/reusables/footer.blade.php
@@ -14,7 +14,7 @@
             <li class="footer-list"><a href="/aboutus">About Us</a></li>
             <li class="footer-list"><a href="/terms-conditions">Terms and Conditions</a></li><!--links to the terms and conditions-->
             <li class="footer-list"><a href = "/student">Student Discount</a></li><!--links to about us-->
-            <li class="footer-list"><a>Privacy Policy</a></li><!--links to about us-->
+            <li class="footer-list"><a href="{{ route('privacy') }}">Privacy Policy</a></li><!--links to about us-->
         </ul>
     </div>
 
@@ -34,5 +34,5 @@
             <li class="footer-list"> <a href="https://x.com/"> <img src="{{asset('images/Insta.png')}}" id = social-pic></li>
         </ul>
     </div>
-    
+
 </footer>
