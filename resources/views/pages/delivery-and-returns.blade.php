c802695050b51626cc66d9319f81c5c78cdae4b7
Matthew Speake <matthew@speake.uk>
2025-02-23 23:06:54 +0000

    cleanup delivery and returns more

--- resources/views/pages/delivery-and-returns.blade.php
+++ resources/views/pages/delivery-and-returns.blade.php
@@ -21,55 +21,58 @@
             <section id="delivery-info" class="content-section">
                 <h2>
                     <img src="{{ asset('images/delivery-truck.png') }}" alt="Delivery Logo" class="section-icon">
-                Delivery Services
-            </h2>
-            <div class="my-3">
+                    Delivery Services
+                </h2>
+                <div class="my-3">
 
-                <ul class="list-group my-4">
-                    <li class="list-group-item">
-                        <strong>Standard Delivery</strong>: We aim to deliver within 4-7 working days. Cost: £4.49.
-                    </li>
-                    <li class="list-group-item">
-                        <strong>Next Day Delivery</strong>: For quick service, items ordered by 7 PM will arrive the
-                        next working day. Cost: £6.49.
-                    </li>
-                    <li class="list-group-item">
-                        <strong>Priority Express Delivery</strong>: Expect delivery within 2-3 business days. Cost:
-                        £5.49.
-                    </li>
-                </ul>
+                    <ul class="list-group my-4">
+                        <li class="list-group-item">
+                            <strong>Standard Delivery</strong>: We aim to deliver within 4-7 working days. Cost: £4.49.
+                        </li>
+                        <li class="list-group-item">
+                            <strong>Next Day Delivery</strong>: For quick service, items ordered by 7 PM will arrive the
+                            next working day. Cost: £6.49.
+                        </li>
+                        <li class="list-group-item">
+                            <strong>Priority Express Delivery</strong>: Expect delivery within 2-3 business days. Cost:
+                            £5.49.
+                        </li>
+                    </ul>
+                </div>
+            </section>
 
-        </section>
-
-        <!-- Returns Section -->
-        <section id="return-info" class="content-section" style="display: none;">
-            <h2 style="font-size: 2em; margin-bottom: 20px;">
-                DO YOU NEED TO RETURN YOUR ORDER?
-            </h2>
-            <p style="font-size: 1.1em; line-height: 1.8; margin-bottom: 20px;">
-                We acknowledge that things don't always go as planned. For your convenience, we have made our returns
-                procedure easy and hassle-free.
-                <br><br>
-                <strong>Returns Procedure:</strong> Only the postal service is used to handle returns. The return
-                details are with your package.
-                For help, you can also get in touch with us directly.
-                <br><br>
-                <strong>Call Us:</strong><br>
-                Contact: 0121 898 919<br>
-                Email: <a href="mailto:support@thesportswear.website">support@thesportswear.website</a>
-                <br><br>
-                <strong>Return Policy:</strong> Any item may be returned within 28 days of order receipt.
-                <br><br>
-                Only the postal service is used to handle returns. The return details are with your box. For help, you
-                can also get in touch with us directly.
-                <br><br>
-                <strong>Refunds:</strong> Following processing, refunds may take up to three to five business days to
-                show up on your original payment method.
-                <br><br>
-                <strong>Important:</strong> The products need to be in their original packing and condition.
-            </p>
-        </section>
-    </div>
+            <!-- Returns Section -->
+            <section id="return-info" class="content-section" style="display: none;">
+                <h2 style="font-size: 2em; margin-bottom: 20px;">
+                    DO YOU NEED TO RETURN YOUR ORDER?
+                </h2>
+                <p style="font-size: 1.1em; line-height: 1.8; margin-bottom: 20px;">
+                    We acknowledge that things don't always go as planned. For your convenience, we have made our
+                    returns
+                    procedure easy and hassle-free.
+                    <br><br>
+                    <strong>Returns Procedure:</strong> Only the postal service is used to handle returns. The return
+                    details are with your package.
+                    For help, you can also get in touch with us directly.
+                    <br><br>
+                    <strong>Call Us:</strong><br>
+                    Contact: 0121 898 919<br>
+                    Email: <a href="mailto:support@thesportswear.website">support@thesportswear.website</a>
+                    <br><br>
+                    <strong>Return Policy:</strong> Any item may be returned within 28 days of order receipt.
+                    <br><br>
+                    Only the postal service is used to handle returns. The return details are with your box. For help,
+                    you
+                    can also get in touch with us directly.
+                    <br><br>
+                    <strong>Refunds:</strong> Following processing, refunds may take up to three to five business days
+                    to
+                    show up on your original payment method.
+                    <br><br>
+                    <strong>Important:</strong> The products need to be in their original packing and condition.
+                </p>
+            </section>
+        </div>
 @endsection
 
 
