c080a9752a0da0d1f56265a539fcf147691614c8
Matthew Speake <matthew@speake.uk>
2025-02-24 11:46:34 +0000

    Cleanup on forgot password
    and css

--- resources/views/pages/forgot-pass.blade.php
+++ resources/views/pages/forgot-pass.blade.php
@@ -1,21 +1,25 @@
 @extends('layouts.page')
 @section('title', 'Forgot Password')
 @section('content')
-   
-<div class="forgot-password">
-    <h1>Have you forgoten your password?</h1>
 
-    <div class="forgot-password-form">
-    <form method="POST" action="{{ route('forgot-pass.change') }}">
-        <label for="email">Enter your email</label>
-        <input type="email" name="email" placeholder="Email" required>
-        
-        <label for="firstInitial lastInitial">Enter your initials for first and last name</label>
-        <input type="text" name="firstInitial" placeholder="Your first initial" maxlength="1" minlength="1" onkeyup="this.value = this.value.toUpperCase();" required>
-        
-        <input type="text" name="lastInitial" placeholder="Your last initial" maxlength="1" minlength="1" onkeyup="this.value = this.value.toUpperCase();" required>
-        <button type="submit">Submit</button>
-    </form>
+    <div class="forgot-password">
+        <h1>Have you forgotten your password?</h1>
+
+        <div class="forgot-password-form">
+            <form method="POST" action="{{ route('forgot-pass.change') }}">
+                <label for="email">Enter your email</label>
+                <input type="email" name="email" placeholder="Email" required>
+
+                <label for="firstInitial lastInitial">Enter your initials for first and last name</label>
+                <input type="text" name="firstInitial" placeholder="Your first initial" maxlength="1" minlength="1"
+                       onkeyup="this.value = this.value.toUpperCase();" required>
+
+                <input type="text" name="lastInitial" placeholder="Your last initial" maxlength="1" minlength="1"
+                       onkeyup="this.value = this.value.toUpperCase();" required>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    </div>
 @endsection
 
 
