f6eb624bc7fd18798fb90f68017a2524cc9b223e
Matthew Speake <matthew@speake.uk>
2025-02-25 17:37:29 +0000

    update composer packages
    people should probably run composer install or composer update at some point

--- resources/views/pages/forgot-pass.blade.php
+++ resources/views/pages/forgot-pass.blade.php
@@ -11,15 +11,11 @@
                 <input type="email" name="email" placeholder="Email" required>
 
                 <label for="firstInitial lastInitial">Enter your initials for first and last name</label>
-                <input type="text" name="firstInitial" placeholder="Your first initial" maxlength="1" minlength="1"
-                       onkeyup="this.value = this.value.toUpperCase();" required>
+                <input type="text" name="firstInitial" placeholder="Your first initial" maxlength="1" minlength="1" onkeyup="this.value = this.value.toUpperCase();" required>
 
-                <input type="text" name="lastInitial" placeholder="Your last initial" maxlength="1" minlength="1"
-                       onkeyup="this.value = this.value.toUpperCase();" required>
+                <input type="text" name="lastInitial" placeholder="Your last initial" maxlength="1" minlength="1" onkeyup="this.value = this.value.toUpperCase();" required>
                 <button type="submit">Submit</button>
             </form>
         </div>
     </div>
 @endsection
-
-
