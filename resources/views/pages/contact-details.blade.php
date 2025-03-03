c93a00cb5cf8028b93004937c1f8944c7c0e94d7
Matthew Speake <matthew@speake.uk>
2025-03-03 16:36:21 +0000

    Add validation to the account details form

--- resources/views/pages/contact-details.blade.php
+++ resources/views/pages/contact-details.blade.php
@@ -1,29 +1,34 @@
 @extends('layouts.page')
 @section('title','Contact Details')
+@section('script', asset('js/contact-details.js'))
 @section('content')
     <div class="contact-details">
         <h1>Your Details</h1>
         <div class="contact-details-form">
             <!-- Needs a controller -->
-            <form method="POST">
+            <form id="details-form">
                 @csrf
                 <h2>First Name</h2>
-                <input type="text" placeholder="What's most important to know?" name="first-name" value="{{ $user['first_name'] }}"></input>
+                <input type="text" id="first-name-new" name="first-name" value="{{ $user['first_name'] }}" required></input>
+                <input type="hidden" id="first-name-old" value="{{ $user['first_name'] }}">
                 <div class="line-break"><br></div>
 
                 <h2>Last Name</h2>
-                <input type="text" placeholder="What's most important to know?" name="last-name" value="{{ $user['last_name'] }}"></input>
+                <input type="text" id="last-name-new" name="last-name" value="{{ $user['last_name'] }}" required></input>
+                <input type="hidden" id="last-name-old" value="{{ $user['last_name'] }}">
                 <div class="line-break"><br></div>
 
                 <h2>Email</h2>
-                <input type="text" placeholder="What's most important to know?" name="email" value="{{ $user['email'] }}"></input>
+                <input type="email" id="email-new" name="email" value="{{ $user['email'] }}" required></input>
+                <input type="hidden" id="email-old" value="{{ $user['email'] }}">
                 <div class="line-break"><br></div>
 
                 <h2>Phone Number</h2>
-                <input type="text" placeholder="What's most important to know?" name="phone-number" value="{{ $user['phone_number'] }}"></input>
-
-                <button type="submit">Submit</button>
+                <input type="text" id="phone-number-new" name="phone-number" value="{{ $user['phone_number'] }}" required></input>
+                <input type="hidden" id="phone-number-old" value="{{ $user['phone_number'] }}">
             </form>
+            <!-- for some reason having hte button in the form was auto-submitting the form -->
+            <button id="submit-button">Update</button>
         </div>
     </div>
 @endsection
