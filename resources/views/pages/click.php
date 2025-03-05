346aa2e62bcfd8b2d708b3392e6ea62cd2073c7b
ShasankThapa <230113480@aston.ac.uk>
2025-03-05 02:03:59 +0000

    Sponsership Page Html
    Added a Sponsership HTML

--- resources/views/pages/click.php
+++ resources/views/pages/click.php
@@ -11,4 +11,33 @@
     </section>
 </div>
 
+<div>
+    <h2> Join our Sponsership Program</h2>
+    <p>Want to Succeed? Want to Achieve you dream? We understand that success is not something that happens in isolation, its about building meaninful relationships with eachother that helps elevate everyone who is involved. Boost your own career with the helping hand of a company that is dedicated to making sure you reach the greatest heights of your career. At Sportswear, we are not only a sports fashion brand, but also your partner that wants to see you achieve your dreams. We are commited to empowering all our atheletes with the tools, support and opportunites that they require to reach their new heights <br> When you work with us, you dont just get yourself a sponser- you gain a true ally in our journey together to success. So how about it? Lets Create something amazing together! </p>
+</div>
+
+<div>
+    <h2> Why Partner With Us?</h2>
+        <li><strong>Proven Marketting Success:</strong> With SportsWear you will have a dedicated marketing team, that are commited to providng our atheletes with the greatest opporutnies available.  </li>
+        <li><strong>Global Reach & Exposure:</strong> As SportsWear is a growing fashion icon, we have a strong presence over various different countries, with a rapid growing community full of active atheletes and fitness enjoyers.</li>
+        <li><strong>Innovative Products:</strong> Our cutting edge clothing wear designs set us apart, allowing us to deliver execptional performance with unparalleled styl made for comfort.</li>
+</div>
+
+<div>
+    <h2> Look at What Our Atheletes Say</h2>
+        <li> </li>
+        <li> </li>
+        <li> </li>
+        <li> </li>
+        <li> </li>
+        <li> </li>
+</div>
+
+<div >
+    <h2 >How to Join Us</h2>
+    <p >If you are interested in exploring sponsorship or investment opportunities with SportsWear, please get in touch with us through the following:</p>
+    <ul>
+        <li><strong>Contact:</strong> [Email Address]</li>
+        <li><strong>Phone Number:</strong> [Phone Number]</li>
+    </ul>
 @endsection
