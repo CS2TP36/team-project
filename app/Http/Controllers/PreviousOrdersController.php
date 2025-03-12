db7e8ab94db11c22a7482ad77e863bf54621e576
Matthew Speake <matthew@speake.uk>
2025-03-12 22:23:02 +0000

    Previous orders error route fix

--- app/Http/Controllers/PreviousOrdersController.php
+++ app/Http/Controllers/PreviousOrdersController.php
@@ -13,7 +13,7 @@ class PreviousOrdersController extends Controller
     // loads the page with the default 10 orders
     function show() {
         if (!Auth::check()) {
-            return redirect('/login/previous-orders')->with('error', 'You are not logged in');
+            return redirect('/login/orders')->with('error', 'You are not logged in');
         }
         $user = Auth::user();
         $orders = Order::where('user_id', $user->id)->orderBy('created_at', 'desc')->paginate(10);
