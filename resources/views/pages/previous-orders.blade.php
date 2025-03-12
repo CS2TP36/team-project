fb3b4ff7da1f7f3a26ff27f5234b14b5ab0ab994
AshfaqC <ashfaqc@outlook.com>
2025-03-12 12:54:56 +0000

    redid the previous orders page
    improved the CSS and added the style and formatting that was asked by the team.
    Moved buttons so they were next to the images
    chnaged buttons to black so matches the theme

--- resources/views/pages/previous-orders.blade.php
+++ resources/views/pages/previous-orders.blade.php
@@ -7,43 +7,48 @@
     @php($orders = PreviousOrders::getPreviousOrders(Auth::user()))
 
     <!-- main container -->
-    <div id="previous-orders-body">
-        <div id="previous-orders">
-            <h1>My Previous Orders</h1>
-            @if($orders->count() > 0)
-                <!--order list section -->
-                <div class="orders-list">
-                    @foreach($orders->reverse() as $order)
-                        <div class="order">
-                            <div class="order-info">
-                                <p class="order-date">Order placed: {{ $order["created_at"] }}</p>
-                                <p class="dispatch-to">Dispatched to: {{ Shipping::all()->where('id', $order["shipping_id"])->first()["delivery_address"] }}</p>
-                                <p class="order-price">Total: £{{ number_format($order->calculateTotal(),2) }}</p>
-                            </div>
-
-                            <div class="order-items">
-                                @foreach($order->individualOrders as $individualOrder)
-                                    <!-- Order 1 details-->
-                                    <img src="{{$individualOrder->product()->getMainImage()}}">
-                                    <div class="previous-item-details">
-                                        <p class="item-name">{{$individualOrder->product()->name}}</p>
-                                        <p class="item-price">£{{ number_format($individualOrder->getSubtotal(), 2) }}</p>
-                                    </div>
-                                @endforeach
-                            </div>
-
-                            <div class="order-actions">
-                                <!-- buttons for order 1 -->
-                                <button class="button">Buy again</button>
-                                <button class="button">Order Details</button>
-                                <button class="button">Leave a review</button>
-                            </div>
+    <div id="previous-orders">
+        <h1>My Previous Orders</h1>
+        
+        @if($orders->count() > 0)
+            <!-- Order list section -->
+            <div class="orders-list">
+                @foreach($orders->reverse() as $order)
+                    <div class="order">
+                        
+                    <!-- order info including total price, dispatched to adn time order was placed  -->
+                    <div class="order-info">
+                            <p class="order-date">Order placed: {{ $order["created_at"] }}</p>
+                            <p class="dispatch-to">Dispatched to: {{ Shipping::all()->where('id', $order["shipping_id"])->first()["delivery_address"] }}</p>
+                            <p class="order-price">Total: £{{ number_format($order->calculateTotal(),2) }}</p>
                         </div>
-                    @endforeach
-            @else
-                <p>No previous orders found</p>
-            @endif
-        </div>
-    </div>
+
+                        <div class="order-items">
+                            @foreach($order->individualOrders as $individualOrder)
+                                <div class="item">
+                                    <img src="{{ $individualOrder->product()->getMainImage() }}" alt="Product Image">
+                        
+                        <!-- product info(name,price and quantity bought) -->
+                        <div class="previous-item-details"> 
+                                        <p class="item-name">{{ $individualOrder->product()->name }}</p> <!-- php to retreive the info-->
+                                        <p class="item-price">£{{ number_format($individualOrder->getSubtotal(), 2) }}</p>
+                                        <p class="item-quantity">Quantity: {{ $individualOrder->quantity }}</p>
+
+                        <!-- buttons for each order -->
+                                        <div class="order-actions"> 
+                                            <button class="button">Buy again</button>
+                                            <button class="button">Leave a review</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            @endforeach
+                        </div> 
+                    </div> 
+                @endforeach
+            </div> 
+        @else
+            <p>No previous orders found</p>
+        @endif
+    </div> 
 
 @endsection
