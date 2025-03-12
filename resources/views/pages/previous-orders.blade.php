e9a1a25ce4dd254f77c66a5108e0a86246d9d762
Matthew Speake <matthew@speake.uk>
2025-03-12 20:40:25 +0000

    Wrote javascript to add more orders to the end of the page

--- resources/views/pages/previous-orders.blade.php
+++ resources/views/pages/previous-orders.blade.php
@@ -10,7 +10,7 @@
         @if($orders->count() > 0)
 
             <!-- Order list section -->
-            <div class="orders-list">
+            <div class="orders-list" id="orders-list">
                 @include('reusables.previous-orders')
             </div>
             @if ($orders->hasMorePages())
@@ -21,3 +21,30 @@
         @endif
     </div>
 @endsection
+
+<script>
+    // the script for loading more pages of orders (need to do it on page as it will use some blade variables)
+    // first wait for the page to load
+    document.addEventListener('DOMContentLoaded', function () {
+        // get the load more button
+        const loadMore = document.getElementById('load-more');
+        // add an event listener to the button
+        loadMore.addEventListener('click', function() {
+            // get the current page number using parseInt to convert it to a number
+            let pageNo = parseInt(this.getAttribute('data-page')) || 1;
+            // make a fetch request to the server to get the next set of orders
+            fetch(`/orders/more?page=${pageNo}`, {
+                headers: {
+                    'X-Requested-With': 'XMLHttpRequest'
+                }
+                // put hte response into the page
+            }).then(response => response.text()).then(html => {
+                // put received html inside the orders list after the current orders
+                document.getElementById('orders-list').insertAdjacentHTML('beforeend', html);
+                // increment the page number
+                this.setAttribute('data-page', pageNo + 1);
+                // catch errors
+            }).catch(error => console.error(error));
+        });
+    });
+</script>
