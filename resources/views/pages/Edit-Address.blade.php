8a0bcc8c952af15fdbd6c9000b7e095c50089f26
K3YC0D3 <firstnamesurename8@gmail.com>
2025-03-12 16:28:42 +0000

    Update Edit-Address.blade.php

--- resources/views/pages/Edit-Address.blade.php
+++ resources/views/pages/Edit-Address.blade.php
@@ -7,25 +7,25 @@
         <h2> Edit Address </h2>
         <form id = "editAddressForm">
 
-        <label for ="full_Name">FULL NAME (first & last)</label>
+        <label for ="full_Name">Full Name (first & last)</label>
                 <input type = "text" id="full_Name"name="full_Name"placeholder="Enter your name" required>
             
-            <label for ="phone_Number">PHONENUMBER</label>
+            <label for ="phone_Number">Phone Number</label>
                 <input type = "text" id="phone_Number" name="phone_Number"placeholder= "Enter your Phone number" required>
 
-            <label for ="post_Code">POSTCODE</label> 
+            <label for ="post_Code">Post Code</label> 
                 <input type = "text" id="post_Code"name="post_Code"placeholder = " Enter your PostCode" required>
 
-            <label for ="address_Line1">ADDRESSLINE1 (Company Name)</label>
+            <label for ="address_Line1">Address Line 1 (Company Name)</label>
                 <input type = "text"id="address_Line1"name="address_Line1" placeholder = "Enter your Address line" required>
             
-            <label for ="address_Line2">ADDRESSLINE2 (Optional)</label>
+            <label for ="address_Line2">Address Line 2 (Optional)</label>
                 <input type = "text"id="address_Line2"name="address_Line2" placeholder = "Enter your Address line ">
 
-            <label for ="town_City">TOWN/CITY</label>
+            <label for ="town_City">Town/City</label>
                 <input type = "text"id="town_City"name="town_City" placeholder = "Enter your Town/City" required>
 
-            <label for ="county">COUNTY (If Applicable)</label> 
+            <label for ="county">County (If Applicable)</label> 
                 <input type = "text"id="county"name="county" placeholder = "Enter your County">
 
             <div class = "checkbox-container">
