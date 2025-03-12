bc5278146d5ad9f27e70db8c693f4f06193cdecc
Matthew Speake <matthew@speake.uk>
2025-03-12 21:16:46 +0000

    Create link to admin pages from account

--- resources/views/pages/account.blade.php
+++ resources/views/pages/account.blade.php
@@ -38,6 +38,16 @@
                         <p>Manage password, email and mobile number</p>
                     </div>
                 </a>
+                @if(Auth::user()->isAdmin())
+                    <!-- Link to Admin Page -->
+                    <a href="/admin" class="card">
+                        <img src="" alt="Admin">
+                        <div class="card-content">
+                            <h2>Admin</h2>
+                            <p>The admin pages</p>
+                        </div>
+                    </a>
+                @endif
             </div>
         </div>
     </div>
