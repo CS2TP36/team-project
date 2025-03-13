129b2c6f988f36743d00759eca27fa228cfdf870
rikeshpatel19 <rkpatel1738@gmail.com>
2025-03-13 22:59:43 +0000

    Change Password Page Finalisation
    Finalised the Light Mode and Dark Mode for the page.

--- resources/views/pages/change-pass.blade.php
+++ resources/views/pages/change-pass.blade.php
@@ -1,11 +1,9 @@
 @extends('layouts.page')
 @section('title', 'Forgot-pass')
 @section('content')
-
     <div class="change">
-        <h2>Change Your Password</h2>
-
         <div class="reset-sections">
+            <h2>Change Your Password</h2>
             <h3>Need to change your password? Please enter a new password below: </h3>
 
             @if ($errors->any())
@@ -20,11 +18,10 @@
 
             <form method="POST" action="{{ route('change-pass.change') }}">
                 @csrf
-                <label for="password">New Password</label><br>
+                <div class="change-pass-label"><label for="password">New Password *</label><br></div>
                 <input type="password" id="password" name="password" required/><br>
-                <label for="password_confirmation">Confirm</label><br>
+                <div class="change-pass-label"><label for="password_confirmation">Confirm *</label><br></div>
                 <input type="password" id="password_confirmation" name="password_confirmation" required/><br>
-
                 <button type="submit">Change Password</button>
             </form>
         </div>
