fdcdfb8b61ad793695dc177f17782366d63d94c3
rikeshpatel19 <rkpatel1738@gmail.com>
2025-03-13 13:18:57 +0000

    Sorted out Review header
    Review header was not put in the right place, now it moves with the rest of the form.

--- resources/views/pages/review.blade.php
+++ resources/views/pages/review.blade.php
@@ -2,9 +2,8 @@
 @section('title','Review')
 @section('content')
     <div class="review">
-        <h1>Create Review</h1>
-        <!-- Need a form submission -->
         <div class="review-form">
+            <h1>Create Review</h1>
             <form method="POST" action="{{ route('review.add') }}">
                 @csrf
                 <!-- Image + Name of Product -->
@@ -12,7 +11,7 @@
                     <img src="{{asset($product->getMainImage())}}" alt="Product Image">
                     <p>{{$product->name}}</p>
                 </div>
-                <div class="line-break"><br></div>
+                <div class="review-line-break"><br></div>
                 <h2>Overall rating</h2>
                 <!-- Stars -->
                 <div class="rating" data-rating="0">
@@ -23,17 +22,17 @@
                     <span class="star" data-value="5">&#9734;</span>
                 </div>
                 <script src="{{ asset('js/rating.js') }}"></script>
-                <div class="line-break"><br></div>
+                <div class="review-line-break"><br></div>
                 <h2>Add a headline</h2>
                 <!-- Input for headline -->
                 <input type="text" placeholder="What's most important to know?" name="headline"></input>
-                <div class="line-break"><br></div>
+                <div class="review-line-break"><br></div>
                 <h2>Add a written review</h2>
                 <!-- Input for review -->
                 <textarea name="message" id="message" cols="10" rows="7"
                         placeholder="What did you like or dislike? What did you use this product for?" required
                         style="resize: none"></textarea>
-                <div class="line-break"><br></div>
+                <div class="review-line-break"><br></div>
                 <p>We will notify you via email as soon as your review is processed.</p>
                 <input type="hidden" name="product_id" value="{{$product->id}}">
                 <input type="hidden" id="rating-holder" name="rating" value="0">
