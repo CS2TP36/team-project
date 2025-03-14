77b06decea48ec55e2e8b6e3bfd3faa5e1d44c44
rikeshpatel19 <rkpatel1738@gmail.com>
2025-03-14 15:22:13 +0000

    Finalised Forgot Password Page
    Got rid of old CSS, made this page look similar to the Change Password Page for consistency.

--- resources/views/pages/forgot-pass.blade.php
+++ resources/views/pages/forgot-pass.blade.php
@@ -3,24 +3,21 @@
 @section('content')
 
     <div class="forgot-password">
-        <h1>Have you forgotten your password?</h1>
+        <h2>Have you forgotten your password?</h1>
 
         <div class="forgot-password-form">
             <form method="POST" action="{{ route('forgot-pass.change') }}">
-                <label for="email">Enter your email</label>
-                <input type="email" name="email" placeholder="Email" required>
+                <div class="forgot-pass-label"><label for="email">Enter your email *</label></div>
+                <input type="email" name="email" placeholder="Email" required>   
 
-                <label for="firstInitial lastInitial">Enter your initials for first and last name</label>
+                <div class="forgot-pass-label"><label for="firstInitial lastInitial">Enter your initials for first and last name *</label></div>
                 <input type="text" name="firstInitial" placeholder="Your first initial" maxlength="1" minlength="1"
                        onkeyup="this.value = this.value.toUpperCase();" required>
 
                 <input type="text" name="lastInitial" placeholder="Your last initial" maxlength="1" minlength="1"
                        onkeyup="this.value = this.value.toUpperCase();" required>
 
-                <div class="button">
-                    <button type="submit">Submit</button>
-                </div>
-
+                <button type="submit">Submit</button>
             </form>
         </div>
     </div>
