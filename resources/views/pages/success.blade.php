c1ddb9cdd816f5f7b27209b9e332e5f2286a9f8b
ShasankThapa <230113480@aston.ac.uk>
2025-03-15 20:38:38 +0000

    Success Order CSS

--- resources/views/pages/success.blade.php
+++ resources/views/pages/success.blade.php
@@ -3,11 +3,12 @@
 @section('content')
     <!-- checks if there is an order and tracking number provided -->
     @if(isset($orderNumber) and  isset($trackingNumber))
-        <div class="aboutus">
-            <h1>Order Placed</h1>
+        <div class="order-success">
+            <h1>Order Has Been Placed</h1>
             <p>Order number: {{ $orderNumber }}</p>
             <br>
             <p>Tracking number: {{$trackingNumber}}</p>
+            <h2>Thanks For Uuing SportsWear, we hope you enjoy your clothing!</h2>
         </div>
     @endif
 @endsection
