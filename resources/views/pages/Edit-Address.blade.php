7a7e213fc399cff106bd4a68eef91aeb4b1548e3
LOKI-HGR <h.afzal.786@outlook.com>
2025-03-18 22:10:17 +0000

    Update Edit-Address.blade.php
    Sorting code

--- resources/views/pages/Edit-Address.blade.php
+++ resources/views/pages/Edit-Address.blade.php
@@ -7,43 +7,32 @@
     <div class="container">
         <h2>Edit Address</h2>
 
-        <form id="editAddressForm" 
-              action="{{ route('address.update', $address->id) }}" 
-              method="POST">
-            @csrf
-            @method('PUT')
+        <form id="editAddressForm"  action="{{ route('address.update', $address->id) }}" method="POST">
+            @csrf@method('PUT')
 
             <label for="full_Name">Full Name (first & last)</label>
-            <input type="text" id="full_Name" name="full_name" 
-                   value="{{ old('full_name', $address->full_name) }}" required>
+            <input type="text" id="full_Name" name="full_name" value="{{ old('full_name', $address->full_name) }}" required>
 
             <label for="phone_Number">Phone Number</label>
-            <input type="text" id="phone_Number" name="phone_number" 
-                   value="{{ old('phone_number', $address->phone_number) }}" required>
+            <input type="text" id="phone_Number" name="phone_number" value="{{ old('phone_number', $address->phone_number) }}" required>
 
             <label for="post_Code">Post Code</label>
-            <input type="text" id="post_Code" name="post_code" 
-                   value="{{ old('post_code', $address->post_code) }}" required>
+            <input type="text" id="post_Code" name="post_code" value="{{ old('post_code', $address->post_code) }}" required>
 
             <label for="address_Line1">Address Line 1</label>
-            <input type="text" id="address_Line1" name="address_line1" 
-                   value="{{ old('address_line1', $address->address_line1) }}" required>
+            <input type="text" id="address_Line1" name="address_line1" value="{{ old('address_line1', $address->address_line1) }}" required>
 
             <label for="address_Line2">Address Line 2 (Optional)</label>
-            <input type="text" id="address_Line2" name="address_line2" 
-                   value="{{ old('address_line2', $address->address_line2) }}">
+            <input type="text" id="address_Line2" name="address_line2" value="{{ old('address_line2', $address->address_line2) }}">
 
             <label for="town_City">Town/City</label>
-            <input type="text" id="town_City" name="town_city" 
-                   value="{{ old('town_city', $address->town_city) }}" required>
+            <input type="text" id="town_City" name="town_city" value="{{ old('town_city', $address->town_city) }}" required>
 
             <label for="county">County (If Applicable)</label>
-            <input type="text" id="county" name="county" 
-                   value="{{ old('county', $address->county) }}">
+            <input type="text" id="county" name="county" value="{{ old('county', $address->county) }}">
 
             <div class="checkbox-container">
-                <input type="checkbox" id="is_default" name="is_default" value="1"
-                    {{ old('is_default', $address->is_default) ? 'checked' : '' }}>
+                <input type="checkbox" id="is_default" name="is_default" value="1" {{ old('is_default', $address->is_default) ? 'checked' : '' }}>
                 <label for="is_default">Set As Default Address</label> 
             </div>
 
