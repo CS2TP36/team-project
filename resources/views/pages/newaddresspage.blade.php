ff74ff94a63269207810b6f6e2eb485645c0fd04
LOKI-HGR <h.afzal.786@outlook.com>
2025-03-18 17:38:40 +0000

    Update newaddresspage.blade.php
    Made changes to allow for easier error handelling

--- resources/views/pages/newaddresspage.blade.php
+++ resources/views/pages/newaddresspage.blade.php
@@ -1,30 +1,39 @@
 @extends('layouts.page')
-
 @section('title', 'New Address')
-
+@section('script', asset('js/newaddress.js'))
 @section('content')
 <div class="new-address-container">
-    <form class="address-form" action="{{ route('address.store') }}" method="POST">
+    <form id="newaddressForm" action="{{ route('address.store') }}" method="POST">
         @csrf
         <h2>Add a New Address</h2>
+        
+        <div class="form-group">
+            <label for="full-name">Full Name</label>
+            <input type="text" id="full-name" name="full_name" placeholder="Enter your full name" required>
+        </div>
 
-        <label for="full-name">Full Name</label>
-        <input type="text" id="full-name" name="full_name" placeholder="Enter your full name" required>
+        <div class="form-group">
+            <label for="phone">Phone Number</label>
+            <input type="tel" id="phone" name="phone_number" placeholder="Enter your phone number" required>
+        </div>
 
-        <label for="phone">Phone Number</label>
-        <input type="tel" id="phone" name="phone_number" placeholder="Enter your phone number" required>
+        <div class="form-group">
+            <label for="postcode">Postcode</label>
+            <input type="text" id="postcode" name="post_code" placeholder="Enter your postcode" required>
+        </div>
 
-        <label for="postcode">Postcode</label>
-        <input type="text" id="postcode" name="post_code" placeholder="Enter your postcode" required>
-
-        <label for="address-line1">Address Line 1</label>
-        <input type="text" id="address-line1" name="address_line1" placeholder="Street address, P.O. box, company name" required>
+        <div class="form-group">
+            <label for="address_line1">Address Line 1</label>
+            <input type="text" id="addressline1" name="address_line1" placeholder="Street address, P.O. box, company name" required>
+        </div>
 
         <label for="address-line2">Address Line 2 (Optional)</label>
         <input type="text" id="address-line2" name="address_line2" placeholder="Apartment, suite, unit, building, floor">
 
-        <label for="city">Town/City</label>
-        <input type="text" id="city" name="town_city" placeholder="Enter your city" required>
+        <div class="form-group">
+            <label for="city">Town/City</label>
+            <input type="text" id="city" name="town_city" placeholder="Enter your city" required>
+        </div>
 
         <label for="county">County (Optional)</label>
         <input type="text" id="county" name="county" placeholder="Enter your county">
