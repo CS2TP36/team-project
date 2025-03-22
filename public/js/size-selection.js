b13ed855f2ea4d41b258e18a4629cffdce05a8d3
Matthew Speake <matthew@speake.uk>
2025-03-22 14:30:47 +0000

    Size selection error message

--- public/js/size-selection.js
+++ public/js/size-selection.js
@@ -16,3 +16,27 @@ function selectSize(size) {
     document.getElementById('size').value = size;
     document.getElementById('wishsize').value = size;
 }
+
+// give a message when the user does not select a size
+document.addEventListener('DOMContentLoaded', function () {
+    let submitBtns = document.querySelectorAll("button[type='submit']");
+    // Add event listener to all submit buttons
+    for (let btn of submitBtns) {
+        btn.addEventListener("click", function (event) {
+            let size = document.getElementById('size').value;
+            //check if size is set
+            if (!size) {
+                // dont submit the form
+                event.preventDefault();
+                let messageArea = document.getElementsByClassName("message-area")[0];
+                messageArea.innerHTML += "<div id=\"message-error\" class=\"error\">\n" +
+                    "                <img src=\"/images/caution-icon.png\"></img>\n" +
+                    "                You have to select a size\n" +
+                    "            </div>";
+                return false;
+            }
+            // submit the form
+            this.form.submit();
+        });
+    }
+});
