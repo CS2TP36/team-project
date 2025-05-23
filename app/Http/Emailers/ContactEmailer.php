4d95d4c8de786a173b21a5db8bb8de54dcbe7c60
Matthew Speake <matthew@speake.uk>
2025-05-23 13:48:24 +0100

    spam messages stopped including https/https

--- app/Http/Emailers/ContactEmailer.php
+++ app/Http/Emailers/ContactEmailer.php
@@ -19,7 +19,7 @@ class ContactEmailer extends Emailer
         $subject = 'SportsWear - Thanks for contacting us!';
         $message = 'Hello ' . $contactItem->name . ', we have received your message and will get back to you as soon as possible. Your message: ' . $contactItem->message;
         // send the email
-        if (!str_contains($message, "http")) {
+        if ((!str_contains($message, "http")) and (!str_contains($message, "www")) and (!str_contains($message, ".ru"))) {
             return $this->sendEmail($to, $subject, $message);
         }
         return true;
